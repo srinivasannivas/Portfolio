66c75cccf230a540eccd3aad9103c1303336d6e6
srinivasannivas <132070116+srinivasannivas@users.noreply.github.com>
2025-03-17 00:00:56 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to My Portfolio</h1>
+      <p>Explore my projects, courses, and internships.</p>
+      <button className="explore-btn">Explore More</button>
+    </div>
+  );
+}
+
+export default Home;
