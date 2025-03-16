b10cb969f38580083e68e139a3cbf4f19eb804d7
srinivasannivas <132070116+srinivasannivas@users.noreply.github.com>
2025-03-17 00:02:17 +0530

    Create Internship.js

---
+++ Internship.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Internship() {
+  return (
+    <div>
+      <h1>Internship</h1>
+      <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="resume-btn">View Resume</a>
+    </div>
+  );
+}
+
+export default Internship;
