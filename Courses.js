f42391cdab186801d81fe5fde50f63345a7f6994
srinivasannivas <132070116+srinivasannivas@users.noreply.github.com>
2025-03-17 00:01:39 +0530

    Create Courses.js

---
+++ Courses.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Courses() {
+  return (
+    <div>
+      <h1>Courses</h1>
+      <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="resume-btn">View Resume</a>
+    </div>
+  );
+}
+
+export default Courses;
