e770d91f98eefbc6c892296fcbf211757a149673
srinivasannivas <132070116+srinivasannivas@users.noreply.github.com>
2025-03-17 00:02:48 +0530

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Contact.css";
+
+function Contact() {
+  return (
+    <div className="contact-container">
+      <h1>Contact Me</h1>
+      <p>Email: example@email.com</p>
+      <p>Phone: +1234567890</p>
+    </div>
+  );
+}
+
+export default Contact;
